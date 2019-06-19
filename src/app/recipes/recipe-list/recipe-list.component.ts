c2353e23c91c48925267aec7c42a1a0ce6e7a44f
cwoolnough <cwoolnoughchamorro@googlemail.com>
2019-06-19 21:20:32 +0100

    adding navigation and event binging with ngif

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+    
   ];
 
   constructor() { }
